a61588ce9eaee7db8c3a712bb456e4c09d0ff98a
John Gerlock <john.gerlock@gmail.com>
2018-02-27 16:51:30 -0500

    WIP remove docker testing for the moment

--- tests/test_docker.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import requests
-
-
-def test_health_response(sbds_http_server):
-    """test server health check response (unhealthy passes too)"""
-    url = '%s/health' % sbds_http_server
-    response = requests.get(url)
-    response.raise_for_status()
